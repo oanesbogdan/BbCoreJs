0b8ff883b68aa3b0e9a042f57ad0939959d6192a
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-09-29 14:56:40 +0200

    #2125 clean src/tb/main.build.js

--- src/tb/main.build.js
+++ src/tb/main.build.js
@@ -1,28 +1,31 @@
-/*... core modules path ... */
-require.config({
-    paths: {
-        "tb.core.Core": "build/toolbar.core",
-        "tb.core.ApplicationManager": "build/toolbar.core",
-        "tb.core.Mediator": "build/toolbar.core",
-        "tb.core.RouteManager": "build/toolbar.core",
-        "tb.core.ViewManager": "build/toolbar.core",
-        "tb.core.ControllerManager": "build/toolbar.core",
-        "tb.core.Utils": "build/toolbar.core"
-    }
-});
+(function () {
+    'use strict';
 
-define(
-    "tb.core",
-    [
-        "tb.core.Core",
-        "tb.core.ApplicationManager",
-        "tb.core.Mediator",
-        "tb.core.RouteManager",
-        "tb.core.ViewManager",
-        "tb.core.ControllerManager",
-        "tb.core.Utils"
-    ],
-    function(bbApi){
-        return bbApi.dump();
-    }
-);
+    require.config({
+        paths: {
+            'tb.core.Core': 'build/toolbar.core',
+            'tb.core.ApplicationManager': 'build/toolbar.core',
+            'tb.core.Mediator': 'build/toolbar.core',
+            'tb.core.RouteManager': 'build/toolbar.core',
+            'tb.core.ViewManager': 'build/toolbar.core',
+            'tb.core.ControllerManager': 'build/toolbar.core',
+            'tb.core.Utils': 'build/toolbar.core'
+        }
+    });
+
+    define(
+        'tb.core',
+        [
+            'tb.core.Core',
+            'tb.core.ApplicationManager',
+            'tb.core.Mediator',
+            'tb.core.RouteManager',
+            'tb.core.ViewManager',
+            'tb.core.ControllerManager',
+            'tb.core.Utils'
+        ],
+        function (bbApi) {
+            return bbApi.dump();
+        }
+    );
+}());
