ff7844a8d607cd930f08e7a86b80d7a2ab594dd3
Charles Rouillon <charles.rouillon@lp-digital.fr>
2014-09-29 17:34:56 +0200

    #2100 - clean src/tb/Exception.js

--- src/tb/Exception.js
+++ src/tb/Exception.js
@@ -1,99 +1,117 @@
-(function (window) {
-    "use strict";
-    require(["bbApi",'Backbone'], function (bbCore, Backbone) {
+require(['Backbone', 'tb.core.Api'], function (Backbone, Api) {
+    'use strict';
 
-        var Exception = Backbone.Model.extend({
-            /**
-             * BundleController contructor
-             *
-             * @returns {undefined}
-             */
-            constructor: function () {
-                this.exception = new Exception();
-                this.name = '';
-                this.message = '';
-                this.params = {};
-                this.stack = this.getStack();
-            },
+    var Exception, throwException;
 
-            defaults: {
-                name : '',
-                message:  '',
-                params : {},
-                stack : this.getStack()
-            },
+    /**
+     * Exception is the base class for all BackBee toolbar exceptions
+     */
+    Exception = Backbone.Model.extend({
+        /**
+         * Default properties
+         */
+        defaults: {
+            name: '',
+            message: '',
+            params: {},
+            stack: this.getStack()
+        },
 
-            /**
-             * Function to build the Exception
-             *
-             * @param {string} name
-             * @param {string} message
-             * @param {object} params
-             * @returns {undefined}
-             */
-            raise: function (name, message, params) {
-                this.name = name;
-                this.message = message;
-                this.params = params || {};
-            },
+        /**
+         * Construct the exception
+         */
+        constructor: function () {
+            this.exception = new Exception();
+            this.name = '';
+            this.message = '';
+            this.params = {};
+            this.stack = this.getStack();
+        },
 
-            /**
-             * Function to build the Exception
-             *
-             * @returns {array}
-             */
-            getStack: function() {
-                var err = new Error(),
-                    stack = err.stack.split("\n"),
-                    cleanStack = stack.slice(4);
+        /**
+         * Set the properties of the exception
+         * @param {string} name
+         * @param {string} message
+         * @param {object} params
+         */
+        raise: function (name, message, params) {
+            this.name = name;
+            this.message = message;
+            this.params = params || {};
+        },
 
-                for (var key in cleanStack) {
+        /**
+         * Gets the stack trace
+         * @returns {array}
+         */
+        getStack: function () {
+            var err = new Error(),
+                stack = err.stack.split("\n"),
+                cleanStack = stack.slice(4),
+                key;
+
+            for (key in cleanStack) {
+                if (cleanStack.hasOwnProperty(key)) {
                     cleanStack[key] = this.parseStackLine(cleanStack[key]);
                 }
-                return cleanStack;
-            },
-
-            /**
-             * Function to stock the Exception in bb.lastError ans bb.errors
-             *
-             * @param {Exception} error
-             * @returns {undefined}
-             */
-            pushError: function (error) {
-                if (bb.errors === undefined) {
-                    bb.errors = [];
-                }
-                bb.errors.push(error);
-                bb.lastError = error;
-            },
-
-            /**
-             * Function to parse a stak trace line
-             *
-             * @param {string} line
-             * @returns {object}
-             */
-            parseStackLine : function (line) {
-                if (line === '') return;
-
-                var splitedLine = line.split('@');
-                var call = splitedLine[0];
-                splitedLine = splitedLine[1].split(':');
-
-                return {
-                    line: splitedLine[2],
-                    file: splitedLine[0] + ':' + splitedLine[1],
-                    call: call
-                };
             }
-        });
 
-        var throwException = function (name, message) {
-            var error = new Exception();
-            error.raise.apply(error, arguments);
-            error.pushError(error);
-            throw(name + ' : ' + message);
+            return cleanStack;
+        },
+
+        /**
+         * Function to stock the Exception in Api.get('errors') and Api.get('lastError')
+         * @param {Exception} error
+         */
+        pushError: function (error) {
+            if (undefined === Api.get('errors')) {
+                Api.register('errors', []);
+            }
+
+            Api.get('errors').push(error);
+            Api.register('lastError', error);
+        },
+
+        /**
+         * Function to parse a stak trace line
+         * @param {string} line  Should be something like <call>@<file>:<lineNumber>
+         * @returns {object}
+         */
+        parseStackLine: function (line) {
+            var splitedLine = line.split('@'),
+                call = line,
+                file = 'undefined',
+                lineNumber = 'undefined';
+
+            if (2 === splitedLine.length) {
+                call = splitedLine[0];
+                splitedLine = splitedLine[1].split(':');
+                if (3 ===  splitedLine.length) {
+                    file = splitedLine[0] + ':' + splitedLine[1];
+                    lineNumber = splitedLine[2];
+                }
+            }
+
+            return {
+                line: lineNumber,
+                file: file,
+                call: call
+            };
         }
-        bb.bbCore.register("Exception", throwException);
-    }(window));
-}(window));
+    });
+
+    /**
+     * Throw a new exception
+     * @param {type} name
+     * @param {type} message
+     */
+    throwException = function (name, message) {
+        var error = new Exception();
+        error.raise.apply(error, arguments);
+        error.pushError(error);
+
+        throw (name + ' : ' + message);
+    };
+
+    Api.register('Exception', throwException);
+});
