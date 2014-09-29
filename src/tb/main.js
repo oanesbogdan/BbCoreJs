c20cab695060b8ed9ffded85c7b2553cc3a28c0f
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-09-29 14:51:37 +0200

    #2126 clean src/tb/main.js

--- src/tb/main.js
+++ src/tb/main.js
@@ -1,29 +1,32 @@
-/*... core modules path ... */
-require.config({
-    paths: {
-        "tb.core.Api": "src/tb/core/Api",
-        "tb.core.ApplicationManager": "src/tb/core/ApplicationManager",
-        "tb.core.ApplicationContainer": "src/tb/core/ApplicationContainer",
-        "tb.core.Mediator": "src/tb/core/Mediator",
-        "tb.core.RouteManager": "src/tb/core/RouteManager",
-        "tb.core.ViewManager": "src/tb/core/ViewManager",
-        "tb.core.ControllerManager": "src/tb/core/ControllerManager",
-        "tb.core.Utils": "src/tb/core/Utils"
-    }
-});
+(function () {
+    'use strict';
 
-define(
-    "tb.core",
-    [
-        "tb.core.Api",
-        "tb.core.ApplicationManager",
-        "tb.core.Mediator",
-        "tb.core.RouteManager",
-        "tb.core.ViewManager",
-        "tb.core.ControllerManager",
-        "tb.core.Utils"
-    ],
-    function(bbApi){
-        return bbApi.dump();
-    }
-);
+    require.config({
+        paths: {
+            'tb.core.Api': 'src/tb/core/Api',
+            'tb.core.ApplicationManager': 'src/tb/core/ApplicationManager',
+            'tb.core.ApplicationContainer': 'src/tb/core/ApplicationContainer',
+            'tb.core.Mediator': 'src/tb/core/Mediator',
+            'tb.core.RouteManager': 'src/tb/core/RouteManager',
+            'tb.core.ViewManager': 'src/tb/core/ViewManager',
+            'tb.core.ControllerManager': 'src/tb/core/ControllerManager',
+            'tb.core.Utils': 'src/tb/core/Utils'
+        }
+    });
+
+    define(
+        'tb.core',
+        [
+            'tb.core.Api',
+            'tb.core.ApplicationManager',
+            'tb.core.Mediator',
+            'tb.core.RouteManager',
+            'tb.core.ViewManager',
+            'tb.core.ControllerManager',
+            'tb.core.Utils'
+        ],
+        function (bbApi) {
+            return bbApi.dump();
+        }
+    );
+}());
