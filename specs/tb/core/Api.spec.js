13c534a7441bdd178c7b7fa0625ebbb9a131ad14
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-09-30 17:38:50 +0200

    remove function scope to use strict statement

--- specs/tb/core/Api.spec.js
+++ specs/tb/core/Api.spec.js
@@ -1,11 +1,9 @@
-(function () {
+define(['tb.core', 'tb.core.Api'], function () {
     'use strict';
 
-    define(['tb.core', 'tb.core.Api'], function () {
-        describe('Testing for jasmine', function () {
-            it('something', function () {
-                expect(true).toBe(true);
-            });
+    describe('Testing for jasmine', function () {
+        it('something', function () {
+            expect(true).toBe(true);
         });
     });
-}());
+});
