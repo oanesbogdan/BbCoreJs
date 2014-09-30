e1affdea7f85e1414918a4106d233a19520f2631
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-09-30 18:26:34 +0200

    #2152 add Exception spec

---
+++ specs/tb/core/Exception.spec.js
@@ -0,0 +1,9 @@
+define(['tb.core', 'tb.core.Exception'], function () {
+    'use strict';
+
+    describe('Exceptions spec', function () {
+        it('something', function () {
+            expect(true).toBe(true);
+        });
+    });
+});
