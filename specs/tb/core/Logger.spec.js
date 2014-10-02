4f47c43c2db21a46a4491509c85267e4ffb7976c
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-10-02 19:45:18 +0200

    #2181 Update log spec

--- specs/tb/core/Logger.spec.js
+++ specs/tb/core/Logger.spec.js
@@ -6,13 +6,48 @@ define(['require', 'tb.core', 'tb.core.Api', 'tb.core.Logger'], function (requir
 
     describe('Exceptions spec', function () {
         it('Logs actions', function () {
-            logger.emergency('This is an emergency.');
 
-            expect(api.get('logs')).toBeDefined();
+            logger.emergency('Test a emergency log.');
             expect(api.get('logs').length).toBe(1);
 
-            logger.info('This is an info.');
+            logger.info('Test a info log.');
             expect(api.get('logs').length).toBe(1);
+        });
+
+        it('Change logs level', function () {
+
+            logger.updateLogLevel(6);
+            logger.notice('Test a notice log with updated log level.');
+            expect(api.get('logs').length).toBe(2);
+
+            logger.restaureLogLevel();
+            logger.warning('Test a warning log with reset log level.');
+            expect(api.get('logs').length).toBe(2);
+
+            logger.updateLogLevel();
+            logger.debug('Test is a debug log with unvailable updated log level.');
+            expect(api.get('logs').length).toBe(2);
+        });
+
+        it('Try custom log level', function () {
+
+            logger.log(2, 'Test a custom log level 2.');
+            expect(api.get('logs').length).toBe(3);
+
+            logger.log('information', 'Test a custom log level 9.');
+            expect(api.get('logs').length).toBe(3);
+        });
+
+        it('Try all miscellious log levels', function () {
+
+            logger.alert('Test is a alert log.');
+            expect(api.get('logs').length).toBe(4);
+
+            logger.critical('Test is a critical log.');
+            expect(api.get('logs').length).toBe(5);
+
+            logger.error('Test is a error log.');
+            expect(api.get('logs').length).toBe(6);
         });
     });
 });
