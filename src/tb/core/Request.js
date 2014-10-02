2129bcb18b9762be3af2ba00d5dab30fda679603
Florian Kroockmann <florian.kroockmann@lp-digital.fr>
2014-10-02 11:26:02 +0200

    #2170 Update Request

--- src/tb/core/Request.js
+++ src/tb/core/Request.js
@@ -1,4 +1,4 @@
-define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
+define('tb.core.Request', ['jsclass'], function () {
     'use strict';
 
     /**
@@ -7,40 +7,16 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
     var Request = new JS.Class({
 
         /**
-         * Schema of request
-         * @type {String}
-         */
-        scheme: 'http',
-
-        /**
-         * Domain of request
-         * @type {String}
-         */
-        domain: '',
-
-        /**
          * Uri of request
          * @type {String}
          */
-        uri: '',
-
-        /**
-         * Base uri of request
-         * @type {String}
-         */
-        baseUrl: '',
+        url: '',
 
         /**
          * Method of request
          * @type {String}
          */
         method: 'GET',
-
-        /**
-         * Query params of request
-         * @type {Object}
-         */
-        queryParams: {},
 
         /**
          * Data of request
@@ -57,46 +33,13 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
         },
 
         /**
-         * Set the scheme of request
-         * @param {String} scheme
+         * Set url of request
+         * @param {String} url
          * @returns {Object} Request
          */
-        setScheme: function (scheme) {
-            this.scheme = scheme;
-
-            return this;
-        },
-
-        /**
-         * Set the domain of request
-         * @param {String} domain
-         * @returns {Object} Request
-         */
-        setDomain: function (domain) {
-            this.domain = domain;
-
-            return this;
-        },
-
-        /**
-         * Set the base url of request
-         * @param {String} baseUrl
-         * @returns {Object} Request
-         */
-        setBaseUrl: function (baseUrl) {
-            this.baseUrl = baseUrl;
-
-            return this;
-        },
-
-        /**
-         * Set the uri of request
-         * @param {String} uri
-         * @returns {Object} Request
-         */
-        setUri: function (uri) {
-            this.uri = uri;
-
+        setUrl: function(url) {
+            this.url = url;
+            
             return this;
         },
 
@@ -107,29 +50,6 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
          */
         setMethod: function (method) {
             this.method = method.toUpperCase();
-
-            return this;
-        },
-
-        /**
-         * Set one query param with name and value
-         * @param {String} name
-         * @param {String} value
-         * @returns {Object} Request
-         */
-        setQueryParam: function (name, value) {
-            this.queryParams[name] = value;
-
-            return this;
-        },
-
-        /**
-         * Set query params of request
-         * @param {Object} queryParams
-         * @returns {Object} Request
-         */
-        setQueryParams: function (queryParams) {
-            this.queryParams = queryParams;
 
             return this;
         },
@@ -165,6 +85,14 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
             this.setHeader('Content-Type', contentType);
             return this;
         },
+        
+        /**
+         * Get the url with query params
+         * @returns {String} url builded 
+         */
+        getUrl: function () {
+            return this.url;
+        },
 
         /**
          * Get the content type of request
@@ -175,60 +103,11 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
         },
 
         /**
-         * Get the scheme of request
-         * @returns {String}
-         */
-        getScheme: function () {
-            return this.scheme;
-        },
-
-        /**
-         * Get the domain of request
-         * @returns {String}
-         */
-        getDomain: function () {
-            return this.domain;
-        },
-
-        /**
-         * Get the uri of request
-         * @returns {String}
-         */
-        getUri: function () {
-            return this.uri;
-        },
-
-        /**
-         * Get the base url of request
-         * @returns {String}
-         */
-        getBaseUrl: function () {
-            return this.baseUrl;
-        },
-
-        /**
          * GEt the method of request
          * @returns {String}
          */
         getMethod: function () {
             return this.method;
-        },
-
-        /**
-         * Get the query param with key
-         * @param {String} key
-         * @returns {Object|null}
-         */
-        getQueryParam: function (key) {
-            return this.queryParams[key] || null;
-        },
-
-        /**
-         * Get the query params of request
-         * @returns {Object}
-         */
-        getQueryParams: function () {
-            return this.queryParams;
         },
 
         /**
@@ -245,24 +124,6 @@ define('tb.core.Request', ['jquery', 'jsclass'], function (jQuery) {
          */
         getHeaders: function () {
             return this.headers;
-        },
-
-        /**
-         * Get the url with query params
-         * @returns {String} url builded 
-         */
-        getUrl: function () {
-            var url = '';
-            if ('' !== this.domain) {
-                url = url + this.scheme + '://' +  this.domain;
-            }
-            url =  '/' + url + this.baseUrl + '/' + this.uri + '/';
-
-            if (false === jQuery.isEmptyObject(this.queryParams)) {
-                url = url + ((url.indexOf('?') === -1) ? '?' : '&') + jQuery.param(this.queryParams);
-            }
-
-            return url;
         }
     });
 
