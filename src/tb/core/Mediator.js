e1a4bcce4348af6f1b6ec0cd09820c61f7a0af20
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-10-13 14:22:04 +0200

    #2254 Creating mediator object

--- src/tb/core/Mediator.js
+++ src/tb/core/Mediator.js
@@ -3,58 +3,85 @@
  * and open the template in the editor.
  */
 
-define('tb.core.Mediator', ['tb.core.Api', 'jquery'], function (Api) {
+define('tb.core.Mediator', ['tb.core.Api'], function (Api) {
     'use strict';
 
-    var // containers = {},
+    var Component = function topic (callback, context) {
+            this.callback = callback;
+            this.context = context;
+        },
 
-//         events = {},
-
-        /**
-         * [register description]
-         * @param  {[type]} componentName [description]
-         * @param  {[type]} component     [description]
-         * @return {[type]}               [description]
-         */
-//         register = function (componentName, component) {
-//             if (containers.hasOwnProperty(componentName)) {
-//                 return;
-//             }
-//
-//             containers[componentName] = component;
-//         },
-
-        /**
-         * [invoke description]
-         * @param  {[type]} component [description]
-         * @param  {[type]} params    [description]
-         * @return {[type]}           [description]
-         */
-//         invoke = function (component, params) {
-//             var dfd = new jQuery.Defered();
-//
-//             console.log(component);
-//             console.log(params);
-//
-//             return dfd.promise();
-//         },
-
-        Mediator = {
-            register: function (topic, callback) {
-                console.log(topic);
-                console.log(callback);
-                return;
-            },
-            invoke: function () {
-                return;
-            },
-            publish: function (topic, params, save) {
-                console.log(topic);
-                console.log(params);
-                console.log(save);
-                return;
-            }
+        Mediator = function mediator () {
+            this.topics = {};
+            this.publicated = {};
         };
+
+    Component.prototype.execute = function componentExecution () {
+        if (this.context === undefined) {
+            this.callback.apply(undefined, arguments);
+        } else {
+            this.callback.apply(this.context, arguments);
+        }
+    };
+
+    Mediator.prototype.subscribe = function mediatorSubscribe (topic, callback, context) {
+        var component = new Component(callback, context);
+
+        if (!this.topics.hasOwnProperty(topic)) {
+            this.topics[topic] = [];
+        }
+
+        this.topics[topic].push(component);
+
+        if (this.publicated.hasOwnProperty(topic)) {
+            component.execute(this.publicated[topic].args);
+        }
+    };
+
+    Mediator.prototype.unsubscribe = function mediatorUnsubscribe (topic, callback, context) {
+        var component = new Component(callback, context),
+            i;
+
+        if (this.topics.hasOwnProperty(topic)) {
+            for (var i = 0; i < this.topics[topic].length; i = i + 1) {
+                if(this.topics[topic][i] === component) {
+                    this.topics[topic].splice(i, 1);
+                }
+            }
+        }
+    };
+
+    Mediator.prototype.publish = function mediatorPublish () {
+        var args = Array.prototype.slice.call(arguments),
+            topic = args.shift(),
+            i;
+
+        if(!this.topics.hasOwnProperty(topic)) {
+            for(i = 0; i < this.topics[topic].length; i = i + 1) {
+                try {
+                    this.topics[topic][i].execute(args);
+                } catch {
+                    Api.logger.warning();
+                }
+            }
+        }
+    };
+
+    Mediator.prototype.percistantPublish = function mediatorPublish () {
+        var args = Array.prototype.slice.call(arguments),
+            topic = args.shift();
+
+        this.publish.apply(this, arguments);
+
+        this.publicated[topic] = {
+            args: args
+        };
+    };
+
+    Mediator.prototype.removePublish = function mediatorRemovePublication (topic) {
+        this.publicated[topic] = null;
+        delete this.publicated[topic];
+    };
 
     Api.register('Mediator', Mediator);
 
