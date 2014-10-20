4cdfe9990eb80922279ec5a2f24549cc356ba375
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-10-20 18:17:01 +0200

    #2253 controller manager spec creation

---
+++ specs/tb/core/ControllerManager.spec.js
@@ -0,0 +1,11 @@
+define(['require', 'tb.core'], function () {
+    'use strict';
+
+    var api = require('tb.core');
+
+    describe('Controller manager spec', function () {
+        it('', function () {
+            expect(true).toBe(true);
+        });
+    });
+});
