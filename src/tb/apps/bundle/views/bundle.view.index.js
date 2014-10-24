0a4931d15897db8c0f6495ecc729aff9e6eb0b62
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-10-24 11:47:24 +0200

    #2679 Add lincense file and an abstract license into each file of the project

--- src/tb/apps/bundle/views/bundle.view.index.js
+++ src/tb/apps/bundle/views/bundle.view.index.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ *
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
 define(['jquery', 'tb.core.ViewManager', 'text!bundle/tpl/index', 'bundle.repository'], function (jQuery, ViewManager, template, BundleRepository) {
 
     'use strict';
