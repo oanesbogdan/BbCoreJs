bb36e30d8877eef79633ffe1d77490fd6f8312c8
Florian Kroockmann <florian.kroockmann@lp-digital.fr>
2014-11-20 10:46:27 +0100

    #3200 use formbuilder for show scheduling publication

--- src/tb/apps/main/main.js
+++ src/tb/apps/main/main.js
@@ -29,7 +29,7 @@ require.config({
     }
 });
 
-define('app.main', ['tb.core', 'main.view.index', 'jquery', 'component!popin'], function (Core, MainViewIndex, jQuery, Popin) {
+define('app.main', ['tb.core', 'main.view.index', 'jquery', 'jqueryui'], function (Core, MainViewIndex, jQuery) {
     'use strict';
 
     /**
