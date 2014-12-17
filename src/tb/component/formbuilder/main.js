26af8790fd8854221f25b643e13dc58002c20f01
Florian Kroockmann <florian.kroockmann@lp-digital.fr>
2014-12-17 15:33:07 +0100

    #58 Rethink FormBuilder elements

--- src/tb/component/formbuilder/main.js
+++ src/tb/component/formbuilder/main.js
@@ -17,155 +17,127 @@
  * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define('tb.component/formbuilder/main', ['tb.core.Api', 'jquery', 'tb.component/formbuilder/form/Form', 'tb.core.Utils', 'jsclass'], function (Core, jQuery, FormConstructor, Utils) {
-    'use strict';
+define(
+    'tb.component/formbuilder/main',
+    [
+        'tb.core.Api',
+        'jquery',
+        'tb.component/formbuilder/form/Form',
+        'tb.core.Utils',
+        'jsclass'
+    ],
+    function (Core, jQuery, FormConstructor, Utils) {
 
-    /**
-     * FormBuilder object
-     */
-    var formPath = 'src/tb/component/formbuilder/form/',
+        'use strict';
 
-        FormBuilder = new JS.Class({
-            /**
-             *
-             * config.elements:
-             *      name:
-             *          type: 'text'
-             *          label: 'My name'
-             *          value: ''
-             *
-             * @param {type} config
-             */
-            renderForm: function (config) {
+        /**
+         * FormBuilder object
+         */
+        var formPath = 'tb.component/formbuilder/form/',
 
-                var form = {},
-                    dfd = new jQuery.Deferred();
+            FormBuilder = new JS.Class({
+                /**
+                 *
+                 * config.elements:
+                 *      name:
+                 *          type: 'text'
+                 *          label: 'My name'
+                 *          value: ''
+                 *
+                 * @param {type} config
+                 */
+                renderForm: function (config) {
 
-                if (!config.hasOwnProperty('elements')) {
-                    Core.exception('MissingPropertyException', 500, 'Property "elements" not found');
-                }
+                    var form = {},
+                        dfd = new jQuery.Deferred();
 
-                config = jQuery.extend({}, config);
-
-                this.mappingRequire = {};
-                this.mappingTemplate = [];
-
-                this.parseGlobalConfig(config);
-                this.parseFormConfig(config.form);
-
-                form = new FormConstructor(config.form);
-
-                this.parseElementConfig(config.elements, form);
-
-                require.config({paths: this.mappingRequire});
-
-                this.mapTemplate();
-
-                Utils.requireWithPromise(Object.keys(this.mappingRequire)).done(function () {
-                    dfd.resolve(form.render());
-                }).fail(function (e) {
-                    dfd.reject(e);
-                });
-
-                return dfd.promise();
-            },
-
-            parseGlobalConfig: function (config) {
-                if (!config.hasOwnProperty('form')) {
-                    config.form = jQuery.extend({}, {});
-                }
-
-                if (typeof config.onSubmit === 'function') {
-                    config.form.onSubmit = config.onSubmit;
-                }
-
-                if (typeof config.onValidate === 'function') {
-                    config.form.onValidate = config.onValidate;
-                }
-            },
-
-            parseFormConfig: function (formConfig) {
-                var keyFormTemplate,
-                    keyFormView;
-
-                if (!formConfig.hasOwnProperty('template')) {
-                    keyFormTemplate = 'form/template';
-                    this.mappingRequire[keyFormTemplate] = formPath + 'templates/form.twig';
-                    this.mappingTemplate.push(keyFormTemplate);
-                    formConfig.template = keyFormTemplate;
-                }
-
-                if (!formConfig.hasOwnProperty('view')) {
-                    keyFormView = 'form.view';
-                    this.mappingRequire[keyFormView] = formPath + 'views/form.view';
-                    formConfig.view = keyFormView;
-                }
-            },
-
-            parseElementConfig: function (elements, form) {
-                var key,
-                    elementConfig,
-                    typeFormated,
-                    keyClass,
-                    keyTemplate,
-                    keyView;
-
-                for (key in elements) {
-                    if (elements.hasOwnProperty(key)) {
-                        elementConfig = jQuery.extend({}, elements[key]);
-
-                        typeFormated = elementConfig.type.substr(0, 1).toUpperCase() + elementConfig.type.substr(1);
-
-                        //Class
-                        keyClass = 'form.element.' + typeFormated;
-                        elementConfig.class = keyClass;
-                        this.mappingRequire[keyClass] = formPath + 'element/' + typeFormated;
-
-
-                        //Template
-                        if (elementConfig.hasOwnProperty('template')) {
-                            keyTemplate = 'form/element/' + form.getId() + key + '/template';
-                            this.mappingRequire[keyTemplate] = elementConfig.template;
-                        } else {
-                            keyTemplate = 'form/element/' + elementConfig.type + '/template';
-                            this.mappingRequire[keyTemplate] = formPath + 'element/templates/' + elementConfig.type + '.twig';
-                        }
-                        elementConfig.template = keyTemplate;
-                        this.mappingTemplate.push(keyTemplate);
-
-
-                        //View
-                        keyView = 'form.element.view.' + elementConfig.type;
-                        elementConfig.view = keyView;
-                        this.mappingRequire[keyView] = formPath + 'element/views/form.element.view.' + elementConfig.type;
-
-                        form.add(key, elementConfig);
+                    if (!config.hasOwnProperty('elements')) {
+                        Core.exception('MissingPropertyException', 500, 'Property "elements" not found');
                     }
-                }
-            },
 
-            mapTemplate: function () {
-                var key,
-                    mapKey,
-                    keyTemplate;
+                    config = jQuery.extend({}, config);
 
-                for (key in this.mappingTemplate) {
-                    if (this.mappingTemplate.hasOwnProperty(key)) {
-                        for (mapKey in this.mappingRequire) {
-                            if (this.mappingRequire.hasOwnProperty(mapKey)) {
-                                if (this.mappingTemplate.hasOwnProperty(key)) {
-                                    keyTemplate = this.mappingTemplate[key];
-                                    if (mapKey === keyTemplate) {
-                                        this.mappingRequire['text!' + keyTemplate] = this.mappingRequire[mapKey];
-                                        delete this.mappingRequire[keyTemplate];
-                                    }
-                                }
+                    this.mappingRequire = [];
+
+                    this.parseGlobalConfig(config);
+                    this.parseFormConfig(config.form);
+
+                    form = new FormConstructor(config.form);
+
+                    this.parseElementConfig(config.elements, form);
+
+                    Utils.requireWithPromise(this.mappingRequire).done(function () {
+                        dfd.resolve(form.render());
+                    }).fail(function (e) {
+                        dfd.reject(e);
+                    });
+
+                    return dfd.promise();
+                },
+
+                parseGlobalConfig: function (config) {
+                    if (!config.hasOwnProperty('form')) {
+                        config.form = jQuery.extend({}, {});
+                    }
+
+                    if (typeof config.onSubmit === 'function') {
+                        config.form.onSubmit = config.onSubmit;
+                    }
+
+                    if (typeof config.onValidate === 'function') {
+                        config.form.onValidate = config.onValidate;
+                    }
+                },
+
+                parseFormConfig: function (formConfig) {
+                    if (false === formConfig.hasOwnProperty('template')) {
+                        formConfig.template = formPath + 'templates/form.twig';
+                    }
+                    formConfig.template = 'text!' + formConfig.template;
+                    this.mappingRequire.push(formConfig.template);
+
+                    if (false === formConfig.hasOwnProperty('view')) {
+                        formConfig.view = formPath + 'views/form.view';
+                    }
+                    this.mappingRequire.push(formConfig.view);
+                },
+
+                parseElementConfig: function (elements, form) {
+                    var key,
+                        elementConfig,
+                        typeFormated;
+
+                    for (key in elements) {
+                        if (elements.hasOwnProperty(key)) {
+                            elementConfig = jQuery.extend({}, elements[key]);
+
+                            typeFormated = elementConfig.type.substr(0, 1).toUpperCase() + elementConfig.type.substr(1);
+
+                            //Class
+                            if (false === elementConfig.hasOwnProperty('class')) {
+                                elementConfig.class = formPath + 'ElementBuilder!' + typeFormated;
                             }
+                            this.mappingRequire.push(elementConfig.class);
+
+                            //Template
+                            if (false === elementConfig.hasOwnProperty('template')) {
+                                elementConfig.template = formPath + 'element/templates/' + elementConfig.type + '.twig';
+                            }
+                            elementConfig.template = 'text!' + elementConfig.template;
+                            this.mappingRequire.push(elementConfig.template);
+
+                            //View
+                            if (false === elementConfig.hasOwnProperty('view')) {
+                                elementConfig.view = formPath + 'element/views/form.element.view.' + elementConfig.type;
+                            }
+                            this.mappingRequire.push(elementConfig.view);
+
+                            form.add(key, elementConfig);
                         }
                     }
                 }
-            }
-        });
+            });
 
-    return new JS.Singleton(FormBuilder);
-});
+        return new JS.Singleton(FormBuilder);
+    }
+);
